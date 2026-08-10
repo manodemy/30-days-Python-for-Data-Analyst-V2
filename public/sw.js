cbac5dbb3dbe5d7967d190615f00e9ff76fcb25e
Manojkumar <manodamy25@gmail.com>
2026-08-10 22:27:31 +0530

    style(admin): move Save and Publish buttons to top right corner of Configurator box next to heading

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'manodemy-static-cache-v90';
-const DYNAMIC_CACHE_NAME = 'manodemy-dynamic-cache-v90';
+const CACHE_NAME = 'manodemy-static-cache-v91';
+const DYNAMIC_CACHE_NAME = 'manodemy-dynamic-cache-v91';
 
 // Static assets to precache immediately (only truly stable assets)
 const PRECACHE_ASSETS = [
